5960ad3ec33a5cf164821360a296f69d1efc510d
Prateek Jakhar <prtkjakhar@gmail.com>
2020-08-05 21:16:13 +0530

    Updates

---
+++ precache-manifest.463c43ecf19f27c9f4cc1253f6c93658.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be5ff5c414913f619e5005c56d2980e",
+    "url": "/PokeWiki/index.html"
+  },
+  {
+    "revision": "a691b9118fc2ff334374",
+    "url": "/PokeWiki/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "5fa4a787da4c5d186e0a",
+    "url": "/PokeWiki/static/css/main.a579ece5.chunk.css"
+  },
+  {
+    "revision": "a691b9118fc2ff334374",
+    "url": "/PokeWiki/static/js/2.9d162c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokeWiki/static/js/2.9d162c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fa4a787da4c5d186e0a",
+    "url": "/PokeWiki/static/js/main.dde52ebd.chunk.js"
+  },
+  {
+    "revision": "78c52f0171c9b91af09c",
+    "url": "/PokeWiki/static/js/runtime-main.ad5bc699.js"
+  }
+]);
